c2fe22e06e96143ae98b647e66afb3247448188c
Slangeman <fredrik.langa@gmail.com>
2019-01-09 15:45:50 +0100

    Moving over memory game from exercies

---
+++ src/js/Memory.js
@@ -0,0 +1,113 @@
+
+import { memoryTemplate } from './templates.js'
+
+class Memory extends window.HTMLElement {
+  constructor (rows = 4, cols = 4) {
+    super()
+    this.rows = rows
+    this.cols = cols
+    this.tiles = []
+    this.container = document.getElementById('memory-container')
+    this.turn1 = undefined
+    this.turn2 = undefined
+    this.lastTile = undefined
+    this.pairs = 0
+    this.tries = 0
+    this.timeout = undefined
+    this.pairTimeout = undefined
+
+    this.attachShadow({ mode: 'open' })
+    this.shadowRoot.appendChild(memoryTemplate.content.cloneNode(true))
+  }
+
+  connectedCallback () {
+    this.tiles = this.getPicArray(this.rows, this.cols)
+
+    for (let i = 0; i < this.rows * this.cols; i++) {
+      let tile = this.tiles[i]
+      let index = i
+
+      let a = document.createElement('a')
+      a.setAttribute('href', '#')
+      this.shadowRoot.appendChild(a)
+
+      let img = document.createElement('img')
+      img.setAttribute('src', 'image/0.png')
+      a.appendChild(img)
+
+      a.addEventListener('click', (event) => {
+        let img = event.target.nodeName === 'IMG' ? event.target : event.target.firstElementChild
+
+        this.turnBrick(tile, index, img)
+      })
+
+      if ((i + 1) % this.cols === 0) {
+        this.shadowRoot.appendChild(document.createElement('br'))
+      }
+    }
+  }
+
+  turnBrick (tile, index, img) {
+    if (this.turn2) {
+      return
+    }
+    img.src = 'image/' + tile + '.png'
+
+    if (!this.turn1) {
+      this.turn1 = img
+      this.lastTile = tile
+    } else {
+      if (img === this.turn1) {
+        return
+      }
+      this.tries++
+      this.turn2 = img
+
+      if (this.lastTile === tile) {
+        // Found a pair
+        this.pairs++
+
+        if (this.pairs === (this.cols * this.rows) / 2) {
+          console.log('You won on ' + this.tries + ' number of tries!!!')
+        }
+
+        this.pairTimeout = setTimeout(() => {
+          this.turn1.parentNode.classList.add('removed')
+          this.turn2.parentNode.classList.add('removed')
+
+          this.turn1 = null
+          this.turn2 = null
+        }, 200)
+      } else {
+        this.timeout = setTimeout(() => {
+          this.turn1.src = 'image/0.png'
+          this.turn2.src = 'image/0.png'
+
+          this.turn1 = null
+          this.turn2 = null
+        }, 500)
+      }
+    }
+  }
+
+  getPicArray (rows, cols) {
+    let arr = []
+
+    for (let i = 1; i <= (rows * cols) / 2; i++) {
+      arr.push(i)
+      arr.push(i)
+    }
+
+    return this.shuffle(arr)
+  }
+
+  shuffle (a) {
+    for (let i = a.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [a[i], a[j]] = [a[j], a[i]]
+    }
+    return a
+  }
+}
+
+window.customElements.define('memory-game', Memory)
---
+++ src/js/templates.js
@@ -0,0 +1,10 @@
+
+const memoryTemplate = document.createElement('template')
+memoryTemplate.innerHTML = /* html */`
+<link rel="stylesheet" href="css/style.css">
+<div class="memCards">
+</div>
+`
+export {
+  memoryTemplate
+}
