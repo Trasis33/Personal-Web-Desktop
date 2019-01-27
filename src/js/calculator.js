3e59a1f65cee3c2f3b5f8e478c456774435a84aa
Slangeman <fredrik.langa@gmail.com>
2019-01-27 15:35:35 +0100

    setting up eventlistener for buttons

--- src/js/calculator.js
+++ src/js/calculator.js
@@ -11,6 +11,7 @@ export default class Calculator extends window.HTMLElement {
 
     this.container = this.shadowRoot.querySelector('#calculator')
     this.display = this.shadowRoot.querySelector('.calc-display')
+    this.buttons = this.shadowRoot.querySelector('.calc-buttons')
     console.log(this.container, this.display)
 
     this.calcObj = {
@@ -20,10 +21,29 @@ export default class Calculator extends window.HTMLElement {
       display: '0'
     }
     this.updateDisplay()
+    this.calcInput()
   }
 
   updateDisplay () {
     this.display.textContent = this.calcObj.display
+  }
+
+  calcInput () {
+    this.buttons.addEventListener('click', e => {
+      if (e.target.classList.contains('function')) {
+        console.log(e.target.value)
+        return
+      }
+      if (e.target.classList.contains('decimal')) {
+        console.log(e.target.value)
+        return
+      }
+      if (e.target.classList.contains('clear')) {
+        console.log(e.target.value)
+        return
+      }
+      console.log(e.target.value)
+    })
   }
 }
 
